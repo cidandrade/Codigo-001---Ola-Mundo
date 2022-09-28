86a0e739954536268545f06031a307a277ff1748
Cid R. Andrade <profandrade@gmail.com>
2022-09-27 22:49:34 -0300

    Versão 0.0.22.09.27 Código 001 - Olá, Mundo

---
+++ src/br/com/cidandrade/aulas/OlaMundo.java
@@ -0,0 +1,18 @@
+package br.com.cidandrade.aulas;
+
+/**
+ * Esta classe implementa uma aplicação que, simplesmente, escreve "Olá, Mundo"
+ * na saída padrão
+ *
+ * @author cidandrade
+ */
+public class OlaMundo {
+
+    /**
+     * @param args Argumentos da linha de comando
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!!!");
+    }
+
+}
