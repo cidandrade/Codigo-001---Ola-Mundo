52bb4b008839e7398ce921d5340952b36fbd9488
Cid R. Andrade <profandrade@gmail.com>
2022-10-03 08:58:50 -0300

    Versão 1.0.22.10.03 Ajustes em comentarios

--- src/br/com/cidandrade/aulas/OlaMundo.java
+++ src/br/com/cidandrade/aulas/OlaMundo.java
@@ -1,16 +1,13 @@
 package br.com.cidandrade.aulas;
 
 /**
- * Esta classe implementa uma aplicação que, simplesmente, escreve "Olá, Mundo"
+ * Esta aplicação implementa uma aplicação que, simplesmente, escreve "Olá, Mundo"
  * na saída padrão
  *
  * @author cidandrade
  */
 public class OlaMundo {
 
-    /**
-     * @param args Argumentos da linha de comando
-     */
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!!!");
     }
